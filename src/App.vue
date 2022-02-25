2fcb732eb55ea87c87b58a2912164fa9b5aff14b
milica <probojcic21@gmail.com>
2022-02-25 14:49:25 +0100

    Drugi commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="main">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+data() {
+  return {
+    title:'VUE OBUKA'
+  }
+},
+}
+</script>
+
+<style>
+#main {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
